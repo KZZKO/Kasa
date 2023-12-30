3de0fa6264dc22d34cc0196024ab70085794085a
KZKO <med62640@hotmail.fr>
2023-12-30 01:32:43 +0100

    Création fichier json pour les collapse ,Collapse page about terminer

--- src/Components/Collapse/collapse.jsx
+++ src/Components/Collapse/collapse.jsx
@@ -1,7 +1,30 @@
+import React, { useState } from 'react';
+import Statement from '../../Data/Statement.json'
 import './collapse.scss'
 
 export const Collapse = () => {
-    return <div>
-        <p>A Propos</p>
-    </div>
+
+    const [selected, setSelected] = useState(null)
+
+    const toggle = (i) => {
+        if (selected == i) {
+            return setSelected(null)
+        }
+
+        setSelected(i)
+    }
+
+    return <div className='collapse-bloc'>
+            <div className='collapse-elm'>
+                {Statement.map((item, i) => (
+                    <div className='collapse-item'>
+                        <div className='collapse-title' onClick={() => toggle(i)}>
+                            <h2>{item.title}</h2>
+                            <i class={selected === i ? 'fa-solid fa-chevron-up chevron-anim' : 'fa-solid fa-chevron-up'}></i>
+                        </div>
+                        <div className={selected === i ? 'collapse-content collapse-show' : 'collapse-content'}>{item.text}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
 }
